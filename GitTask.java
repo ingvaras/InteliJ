23eefa02f76fcde54bc193b3d8b983ba5765310d
ingvaras <ingvaraz4@gmail.com>
2020-02-06 11:35:08 +0200

    init

--- GitTask.java
+++ GitTask.java
@@ -4,6 +4,8 @@ public class GitTask {
 
     public static void main(String[] args) {
         System.out.println("World!");
+        int i = 435;
+        System.out.println("awdqefo");
         System.out.println();
     }
 }
