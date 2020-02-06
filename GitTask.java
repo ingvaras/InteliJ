ca48fab93695dab5256ff23074c74cd496b420e0
ingvaras <ingvaraz4@gmail.com>
2020-02-06 11:28:56 +0200

    init

--- GitTask.java
+++ GitTask.java
@@ -3,6 +3,6 @@ package com.itacademy.day2_git;
 public class GitTask {
 
     public static void main(String[] args) {
-        System.out.println("Hello Programmer");
+        System.out.println("Hello World!");
     }
 }
