22d8f2cbd62d37a3bddea1ed90eba9418fab026a
ingvaras <ingvaraz4@gmail.com>
2020-02-06 11:45:23 +0200

    Update GitTask.java

--- GitTask.java
+++ GitTask.java
@@ -3,7 +3,7 @@ package com.itacademy.day2_git;
 public class GitTask {
 
     public static void main(String[] args) {
-        System.out.println("Wohgcrld!");
+        System.out.println("aw!");
         System.out.println();
     }
 }
